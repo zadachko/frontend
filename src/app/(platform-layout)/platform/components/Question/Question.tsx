10451e8c4aefaeb6d8d156f683c5eb378d0a9157
ChristianPenev <krunkiemonkey@gmail.com>
2025-08-01 22:13:55 +0300

    Created MultipleChoiceAnswer component

--- src/app/(platform-layout)/platform/components/Question/Question.tsx
+++ src/app/(platform-layout)/platform/components/Question/Question.tsx
@@ -1,14 +1,11 @@
 import { Card, CardContent } from '@/components/ui/card';
-import { Check } from 'lucide-react';
 import React from 'react';
 import 'katex/dist/katex.min.css';
 import QuestionBadge from './QuestionBadge';
 import QuestionStatement from './QuestionStatement';
-import { renderWithMath } from '@/app/(platform-layout)/platform/common/utilities/renderWithMath';
 import OpenAnswer from './OpenAnswer';
 import type { DiagramData } from 'geometry-diagram-renderer';
-
-const optionLetters = ['а', 'б', 'в', 'г', 'д', 'е'];
+import MultipleChoiceAnswer from './MultipleChoiceAnswer';
 
 type QuestionProps = {
     question: {
@@ -42,55 +39,7 @@ const Question = ({ question, answers, handleAnswerChange }: QuestionProps) => {
                     {question.type === 'text' ? (
                         <OpenAnswer answers={answers} handleAnswerChange={handleAnswerChange} questionNumber={question.id} />
                     ) : (
-                        <div className="space-y-3">
-                            <label className="block text-sm font-medium text-gray-700 mb-3">
-                                Изберете своя отговор:
-                            </label>
-                            {question.options?.map(
-                                (option: string, index: number) => {
-                                    const optionLetter = optionLetters[index];
-                                    const isSelected =
-                                        answers[question.id] === optionLetter;
-                                    return (
-                                        <button
-                                            key={index}
-                                            onClick={() =>
-                                                handleAnswerChange(
-                                                    question.id,
-                                                    optionLetter
-                                                )
-                                            }
-                                            className={`w-full text-left p-3 rounded-lg border-2 transition-all duration-200 cursor-pointer ${isSelected
-                                                ? 'border-emerald-500 text-emerald-900'
-                                                : 'border-gray-200 bg-white hover:bg-gray-100/70'
-                                                }`}
-                                        >
-                                            <div className="flex items-center gap-3">
-                                                <div
-                                                    className={`w-4 h-4 rounded-full border-2 flex items-center justify-center ${isSelected
-                                                        ? 'border-emerald-500 bg-emerald-500'
-                                                        : 'border-gray-300'
-                                                        }`}
-                                                >
-                                                    {isSelected && (
-                                                        <Check className="w-4 h-4 text-white" />
-                                                    )}
-                                                </div>
-                                                <span className="font-medium text-gray-700">
-                                                    {optionLetter})
-                                                </span>
-                                                <span className="text-gray-900">
-                                                    {renderWithMath(
-                                                        option,
-                                                        'text-sm'
-                                                    )}
-                                                </span>
-                                            </div>
-                                        </button>
-                                    );
-                                }
-                            )}
-                        </div>
+                        <MultipleChoiceAnswer question={{ ...question, options: question.options! }} answers={answers} handleAnswerChange={handleAnswerChange} />
                     )}
                 </div>
             </CardContent>
