811cdc0762a8b6ffcc7074ac839314d008dfa4d3
Tonkata-hub <tonyyankOV07@gmail.com>
2025-08-03 00:32:58 +0300

    Fixed top navbar dropdowns on mobile and visibility of the sidebar

--- src/app/(platform-layout)/layout.tsx
+++ src/app/(platform-layout)/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
         <div className="flex flex-col h-screen">
           <PlatformNavbar />
           <div className="flex flex-1 overflow-hidden">
-            <div className="h-full sticky top-0">
-              <Sidebar />
-            </div>
             <main className="flex-1 overflow-y-auto">{children}</main>
           </div>
         </div>
