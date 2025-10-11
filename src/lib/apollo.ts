27494044a58894bb881e24ce83b023ca430e53a2
Tonkata-hub <tonyyankOV07@gmail.com>
2025-10-12 00:39:36 +0300

    removed apollo client cache

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -9,8 +9,8 @@ export const makeClient = (): ApolloClient<NormalizedCacheObject> => {
 		// credentials: "include",
 		headers: process.env.NEXT_PUBLIC_GRAPHQL_TOKEN
 			? {
-					Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHQL_TOKEN}`,
-			  }
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHQL_TOKEN}`,
+			}
 			: undefined,
 	});
 
@@ -19,5 +19,15 @@ export const makeClient = (): ApolloClient<NormalizedCacheObject> => {
 	return new ApolloClient({
 		cache: new InMemoryCache(),
 		link: authLink.concat(httpLink),
+		defaultOptions: {
+			watchQuery: {
+				fetchPolicy: 'no-cache',
+				errorPolicy: 'all',
+			},
+			query: {
+				fetchPolicy: 'no-cache',
+				errorPolicy: 'all',
+			},
+		},
 	});
 };
